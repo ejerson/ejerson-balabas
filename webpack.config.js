d9912136515ea29744c97f31e754d698e31010cf
0e0 <ejersonb@gmail.com>
2016-11-14 17:01:59 -0600

    update

---
+++ webpack.config.js
@@ -0,0 +1,69 @@
+const webpack = require('webpack');
+// const HTMLWebpackPlugin = require('html-webpack-plugin');
+// const HTMLWebpackPluginConfig = new HTMLWebpackPlugin({
+//   template: __dirname + '/src/index.html',
+//   filename: 'public/index.html',
+//   inject: 'body'
+// });
+
+var definePlugin = new webpack.DefinePlugin({
+  __DEV__: JSON.stringify(JSON.parse(process.env.BUILD_DEV || 'true')),
+  __PRERELEASE__: JSON.stringify(JSON.parse(process.env.BUILD_PRERELEASE || 'false'))
+});
+
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+
+// Prevent EventEmitter leak warning
+require('events').EventEmitter.prototype._maxListeners = 100;
+
+module.exports = {
+  entry: [
+    './src/index.js'
+  ],
+  output: {
+    path: __dirname,
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['react', 'es2015', 'stage-1']
+        }
+      },
+      {
+        test: /\.less$/,
+        loader: ExtractTextPlugin.extract("style-loader", "css-loader!less-loader")
+      },
+      {
+        test: /\.(jpe?g|png|gif|svg)$/i,
+        loaders: [
+            'file?hash=sha512&digest=hex&name=[hash].[ext]',
+            'image-webpack?bypassOnDebug&optimizationLevel=7&interlaced=false'
+        ]
+    }
+    ]
+  },
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  },
+  devServer: {
+    historyApiFallback: true,
+    contentBase: './'
+  },
+  plugins: [
+    // HTMLWebpackPluginConfig,
+    new ExtractTextPlugin("style/styles.css"),
+    definePlugin,
+    new webpack.optimize.LimitChunkCountPlugin({maxChunks: 15}),
+    new webpack.optimize.UglifyJsPlugin({
+    compress: {
+        warnings: false
+    }
+  })
+  ]
+};
