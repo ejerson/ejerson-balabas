732f1344be684e0d703cb25ec92a9cbc09e63c29
0e0 <ejersonb@gmail.com>
2016-11-15 10:28:39 -0600

    update

--- webpack.config.js
+++ webpack.config.js
@@ -29,7 +29,7 @@ module.exports = {
     loaders: [
       {
         test: /\.js$/,
-        exclude: /node_modules/,
+        exclude: /(node_modules|bower_components)/,
         loader: 'babel-loader'
       },
       {
