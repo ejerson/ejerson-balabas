0630d1547cd40db631213785babb7ef0f838cb92
0e0 <ejersonb@gmail.com>
2016-11-15 09:57:20 -0600

    update

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+import { Router, Route, browserHistory } from 'react-router';
 
 import App from './components/app.js';
 require('./public/styles/index.less');
 
 
-ReactDOM.render(<App />, document.querySelector('.container'));
+ReactDOM.render((
+  <Router history={browserHistory}>
+    <Route path="/" component={App} />
+  </Router>), document.querySelector('.container'));
