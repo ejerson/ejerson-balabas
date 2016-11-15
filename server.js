a405862ee0b487edc28ed875721eb17ca8a1ad54
0e0 <ejersonb@gmail.com>
2016-11-15 14:22:43 -0600

    update

--- server.js
+++ server.js
@@ -4,10 +4,10 @@ const port = process.env.PORT || 8080;
 const app = express();
 
 // /build points my app to this server.
-app.use(express.static(__dirname));
+app.use(express.static('build'));
 
 app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, './build/index.html'));
+  res.sendFile(path.resolve(__dirname, 'index.html'));
 });
 
 
