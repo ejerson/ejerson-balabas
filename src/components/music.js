8e50e7551c85b515db3c5b5b0c47e0934e1036f1
0e0 <ejersonb@gmail.com>
2017-07-10 12:56:14 -0600

    update

--- src/components/music.js
+++ src/components/music.js
@@ -15,7 +15,8 @@ export default class Music extends Component {
         <div className='music-div'>
           <div className='music-philosophy'>
             <h1 className='music-philosophy-title'>philosophy</h1>
-            <h2 className='music-philosophy-body'>Teaching is something that requires patience, understanding, more patience, and the never ending quest to learn. As a teacher, I work to relate the knowledge I have acquired throughout the years,
+            <h2 className='music-philosophy-body'>Teaching is something that requires patience, understanding, more patience, and the never ending quest to learn.
+            As a teacher, I work to relate the knowledge I have acquired throughout the years,
             and ensure that my students learn to become independent learners by providing them with the tools necessary to succeed.
             </h2>
 
