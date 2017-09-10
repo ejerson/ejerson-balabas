81254bcb25eb9fd370cf923e085d667931b7f10a
0e0 <ejersonb@gmail.com>
2017-09-09 20:56:37 -0600

    update

--- src/components/footer.js
+++ src/components/footer.js
@@ -5,7 +5,7 @@ export default React.createClass({
   render() {
     return (
       <div className='footer'>
-        <img className='social-media facebook' src={require('./images/facebook.png')} />
+        <a href='https://www.facebook.com/ejersonb/'><img className='social-media facebook' src={require('./images/facebook.png')} /></a>
         <img className='social-media twitter' src={require('./images/twitter.png')} />
         <img className='social-media linkedin' src={require('./images/linkedin.png')} />
         <img className='social-media medium' src={require('./images/medium.png')} />
