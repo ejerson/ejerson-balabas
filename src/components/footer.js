c0344e2aef81a3ee237a71236da8047c14e867c8
0e0 <ejersonb@gmail.com>
2017-09-09 21:04:51 -0600

    update

--- src/components/footer.js
+++ src/components/footer.js
@@ -5,10 +5,10 @@ export default React.createClass({
   render() {
     return (
       <div className='footer'>
-        <a href='https://www.facebook.com/ejersonb/'><img className='social-media facebook' src={require('./images/facebook.png')} /></a>
-        <img className='social-media twitter' src={require('./images/twitter.png')} />
-        <img className='social-media linkedin' src={require('./images/linkedin.png')} />
-        <img className='social-media medium' src={require('./images/medium.png')} />
+        <a href='https://www.facebook.com/ejersonb/' target='_blank'><img className='social-media facebook' src={require('./images/facebook.png')} /></a>
+        <a href='https://twitter.com/ejerson_' target='_blank'><img className='social-media twitter' src={require('./images/twitter.png')} /></a>
+        <a href='https://www.linkedin.com/in/ejerson-balabas-209b19122/' target='_blank'><img className='social-media linkedin' src={require('./images/linkedin.png')} /></a>
+        <a href='https://medium.com/@ejersonb' target='_blank'><img className='social-media medium' src={require('./images/medium.png')} /></a>
       </div>
     );
   }
