dd999c61d2e62247f3bc6c999e08921b0e94e11d
Ejerson Balabas <ejersonb@gmail.com>
2019-05-25 17:36:36 -0600

    'update'

--- webpack.config.js
+++ webpack.config.js
@@ -47,7 +47,7 @@ module.exports = {
 
         loaders: [
             'file?hash=sha512&digest=hex&name=[name].[ext]',
-            'image-webpack?bypassOnDebug&optimizationLevel=7&interlaced=false'
+            'image-webpack?bypassOnDebug'
         ]
     },
 
